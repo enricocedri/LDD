0c61baf11e2729589d96d470c25786129edce436
enrico.cedri <enrico.cedri@accenture.com>
2018-04-05 12:58:04 +0200

    sw pwa

--- src/sw.js
+++ src/sw.js
@@ -9,7 +9,9 @@ const resToCache = [
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(cacheName).then(cache => cache.addAll(resToCache))
+    caches.open(cacheName)
+        .then(cache => cache.addAll(resToCache))
+        .catch(err => console.log("Errore", err))
   );
 });
 
@@ -29,6 +31,8 @@ self.addEventListener('activate', event => {
           if (key !== cacheName) {
             console.log('removing cache:', key);
             return caches.delete(key);
+          } else {
+              console.log("cache to maintain", cacheName)
           }
         })
       );
